d58e99664c564c42b9289b5f8bc90b9f8161703b
diegopanici <94963563+diegopanici@users.noreply.github.com>
2025-11-21 02:13:19 +0000

    Update app.R

--- app.R
+++ app.R
@@ -27,10 +27,11 @@ ui <- navbarPage("Instream large wood on the River Isonzo", id = 'nav',
 
 # Define server
 server <- function(input, output, session) {
-  source("Server.R", local = environment())
+  source("Server_function.R", local = environment())
 }
 
 # Run the application
 shinyApp(ui, server)
+
 
 
